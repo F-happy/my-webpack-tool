eabc2e80f90fc477b371c4b5de5fe52c7f39362f
付会翔 <fuhuixiang@jonnyf.com>
2016-11-29 11:10:17 +0800

    删除冗余代码

--- mobile/js/index.jsx
+++ mobile/js/index.jsx
@@ -11,20 +11,12 @@ require('../scss/style.scss');
 import 'whatwg-fetch';
 
 import Home from './views/home';
-import LoginView from './views/login';
-import BuyBackCard from './views/buy_back_card';
-import TaskCard from './views/take_card';
-import MyCard from './views/my_card';
 
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/">
-            <IndexRoute component={LoginView}/>
+            <IndexRoute component={Home}/>
             <Route path="/home" component={Home}/>
-            <Route path="/login" component={LoginView}/>
-            <Route path="/buy_back_card" component={BuyBackCard}/>
-            <Route path="/take_card" component={TaskCard}/>
-            <Route path="/my_card" component={MyCard}/>
         </Route>
     </Router>
 ), document.getElementById('duobao'));
---
+++ mobile/js/views/home.jsx
@@ -0,0 +1,19 @@
+/**
+ * Created by fuhuixiang on 2016-11-29.
+ */
+"use strict";
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                hello, world!
+            </div>
+        )
+    }
+}
